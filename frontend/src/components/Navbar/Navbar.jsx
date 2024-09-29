fbf0b227e1bdb3b5fb4fc6e301136f5fbd9ab007
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-09-29 22:07:16 +0530

    Creating Cart and Delivery address , Proceed to checkout features

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./Navbar.css"
 import { assets } from "../../assets/assets"
 import { Link } from "react-router-dom"
+import { StoreContext } from '../../context/StoreContext'
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home")
+    const [cartCount, setCartCount] = useState(0)
+
+    const { cartItems, getCartTotalAmount } = useContext(StoreContext)
+
+    useEffect(() => {
+        let no_of_items = 0
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                no_of_items += 1
+            }
+        }
+        setCartCount(no_of_items)
+    }, [cartItems])
+
+    // console.log(cartCount)
 
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt='' className="logo" />
+            <Link to="/"><img src={assets.logo} alt='' className="logo" /></Link>
             <ul className='navbar-menu'>
                 <Link to="/" href='#' onClick={() => setMenu("home")} className={menu === "home" ? 'active' : ''}>home</Link>
                 <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? 'active' : ''}>menu</a>
@@ -20,10 +36,10 @@ const Navbar = () => {
             <div className='navbar-right'>
                 <img src={assets.search_icon} alt='' />
                 <div className='navbar-search-icon'>
-                    <img src={assets.basket_icon} alt='' />
-                    <div className='dot'></div>
+                    <Link to="/cart"><img src={assets.basket_icon} alt='' /></Link>
+                    <div className={getCartTotalAmount() === 0 ? "" : "dot"}>{getCartTotalAmount() !== 0 ? cartCount : ""}</div>
                 </div>
-                <button>sign in</button>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
 
         </div>
