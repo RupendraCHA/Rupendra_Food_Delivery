30b87b37bbb44a039e3ec46c9909dc29639d76c8
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-02 18:44:35 +0530

    Creating API End Points for Getting all Food Items and Removing Food Item From the Database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,5 +4,5 @@ export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://chandaluri210:R9912192624r@cluster0.rijkr.mongodb.net/RupendraFoodDelivery?retryWrites=true&w=majority&appName=Cluster0')
         .then(() => {
             console.log("DB Connected!!")
-        })
+        }).catch((error) => console.log(error))
 }
