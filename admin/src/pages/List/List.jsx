789beb85c7084fcf08731831459458d0549e5542
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-07 20:00:27 +0530

    Update List.jsx with backend deployed URL

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 
 const List = () => {
 
-    const url = "http://localhost:4000"
+    const url = "https://rupendra-food-delivery.onrender.com"
     const [list, setList] = useState([])
 
     const fetchList = async () => {
