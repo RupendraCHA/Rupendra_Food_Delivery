54d416009b031fe16cdabe7128d8a33057d65dc7
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-07 18:46:05 +0530

    Testing the app for deployment

---
+++ admin/src/tests/example.test.jsx
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest"
+
+describe("Example Suite", () => {
+    it("should equal", () => {
+        const sum = 2 + 2
+        expect(sum).toEqual(4)
+    })
+})
