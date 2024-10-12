9836c6782a6300c7b86e74008f926aaa04ab3674
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-12 12:47:59 +0530

    Creating Login Controller for User to authenticate himself into website in the beginning

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,16 +4,39 @@ import bcrypt from "bcrypt"
 import validator from "validator"
 
 
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
+}
+
 //login user
 export const loginUser = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exist" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid Credentials/Password Incorrect" })
+        }
+
+        const token = createToken(user._id)
+        res.json({ success: true, token })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
+
 
 }
 
 //register user
 
-const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET)
-}
+
 
 export const registerUser = async (req, res) => {
     const { name, email, password } = req.body
