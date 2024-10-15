c6be22cc484a917d85541f0c1a3f0fed59155001
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-15 17:54:46 +0530

    Header Text changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
+                <h2>Choose the most delicious items from Rupendra Foods</h2>
                 <p>Choose from a diverse menu featuring a delectable
                     array of dishes crafted with the finest ingredients and
                     culinary expertise. Our mission is to satisfy your cravings
