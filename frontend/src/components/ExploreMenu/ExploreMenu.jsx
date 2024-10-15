5c8500a030019e141579811c99d04030e54f99ec
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-15 19:01:52 +0530

    Text Alterations

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,7 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({ category, setCategory }) => {
     return (
         <div className='explore-menu' id='explore-menu'>
-            <h1>Explore oue menu</h1>
+            <h1>Explore the menu</h1>
             <p className='explore-menu-text'>
                 Choose from a diverse menu featuring a delectable array of dishes.
                 Our mission is to satisfy your cravings
