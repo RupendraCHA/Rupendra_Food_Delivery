abb707c1986fd7ca1af5fca62d5d16927b40bdbf
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-15 18:26:34 +0530

    Updating UserController.js Backend File

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,7 +70,7 @@ export const registerUser = async (req, res) => {
 
         const user = await newUser.save()
         const token = createToken(user._id)
-        res.json({ success: true, token })
+        res.json({ success: true, token, name })
 
     } catch (error) {
         console.log(error)
