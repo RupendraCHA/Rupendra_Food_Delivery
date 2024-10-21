30a839968fe8e255d3a821f1de22cb84a7e7a96d
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-21 14:55:10 +0530

    Hiding Login information for new user

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Header.css"
+import { StoreContext } from '../../context/StoreContext'
 
 const Header = () => {
+
+    const {token} = useContext(StoreContext)
+
     return (
         <div className='header'>
-            <div className='user'>
-                <p>Login Id: user123@gmail.com</p>
-                <p>Password: 123456789</p>
-            </div>
+            {!token && (
+                <div className='user'>
+                    <p>Login Id: user123@gmail.com</p>
+                    <p>Password: 123456789</p>
+                </div>
+            )}
             <div className='header-contents'>
                 <h2>Choose the most delicious items from Rupendra Foods</h2>
                 <p>Choose from a diverse menu featuring a delectable
